ac848162435ad57dc73c5b41b05bb7b56b21353e
Sachin KM <hiscan99@gmail.com>
2025-09-23 17:32:31 +0200

    chore: update dependencies in package.json
    - Added firebase dependency (version ^12.3.0)
    - Reordered react-native-worklets dependency to maintain consistency
    - Added @react-native-async-storage/async-storage dependency (version 2.2.0)
    - Ensured typescript is listed in devDependencies for clarity

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,8 +1,17 @@
 import { Redirect, Stack } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
 import { useAuth } from "../_layout";
 
 export default function AuthLayout() {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, isLoading } = useAuth();
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
   console.log("is Authenticated", isAuthenticated);
   if (isAuthenticated) {
     return <Redirect href="/(tabs)" />;
