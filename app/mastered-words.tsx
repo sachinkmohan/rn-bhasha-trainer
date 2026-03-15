cded54d67be60fd2b28b93e6a24b284597a9a284
Sachin KM <hiscan99@gmail.com>
2026-03-15 14:51:24 +0100

    feat: enhance MasteredWordsScreen with FlatList for displaying mastered words

--- app/mastered-words.tsx
+++ app/mastered-words.tsx
@@ -1,11 +1,32 @@
 import { useMasteredWords } from "@/hooks/useMasteredWords";
-import { Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 export default function MasteredWordsScreen() {
   const { masteredWords, isLoading } = useMasteredWords();
   console.log("Get MasteredWords", masteredWords);
   return (
-    <View>
-      <Text> Mastered Words Screen</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={masteredWords}
+        renderItem={({ item }) => (
+          <View style={styles.itemRow}>
+            <Text>{item.word.inTranslit}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 16,
+    flex: 1,
+    paddingBottom: 8,
+  },
+  itemRow: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    paddingVertical: 12,
+  },
+});
